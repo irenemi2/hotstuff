861701f3399dd95e3cf9b72dc5422bb89e605e71
Irene Isaac <ireneisaac@gmail.com>
2022-03-24 12:39:00 -0500

    updated graphs

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -100,10 +100,10 @@ def remote(ctx):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'nodes': [4],
-        'rate': [26_000,30_000], #,3_000,4_000,5_000],
+        'rate': [4_000], #,3_000,4_000,5_000],
         'tx_size': 512,
         'duration': 300,
-        'runs': 2,
+        'runs': 1,
     }
     node_params = {
         'consensus': {
@@ -114,7 +114,7 @@ def remote(ctx):
         },
         'mempool': {
             'queue_capacity': 100_000,
-            'max_payload_size': 500_000,
+            'max_payload_size': 160_000,
             'min_block_delay': 100
         }
     }
